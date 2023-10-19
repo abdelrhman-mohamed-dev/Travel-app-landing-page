54fbfbf54e6fcd62da25a0473d761b73edce7c5c
Abdelrhman Mohamed <mohamedsaad81973@gmail.com>
2023-10-19 18:31:32 +0300

    fix scroll  bug

--- components/Camp.tsx
+++ components/Camp.tsx
@@ -73,10 +73,10 @@ const Camp = () => {
             <strong>Feeling Lost</strong> And Not Knowing The Way?
           </h2>
           <p className="regular-14 xl:regular-16 mt-5 text-white">
-            Starting from the anxiety of the climbers when visiting a new
+            {`Starting from the anxiety of the climbers when visiting a new
             climbing location, the possibility of getting lost is very large.
             That's why we are here for those of you who want to start an
-            adventure
+            adventure`}
           </p>
           <Image
             src="/quote.svg"
